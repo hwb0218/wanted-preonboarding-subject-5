eef90abdd53aa36627f0d8a9b6785aca4199b164
WOOBEEN <hwb0218@gmail.com>
2021-08-17 05:22:06 +0900

    chore : App 컴포넌트 SortingMachine으로 변경 및 파일 제거
    Co-authored-by: imhjlov <imhjlov@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import SortingMachine from "./SortingMachine";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<SortingMachine />, document.getElementById("root"));
