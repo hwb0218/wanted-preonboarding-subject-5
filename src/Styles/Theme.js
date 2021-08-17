a28d52557d273df3ebe8eee290307897f21ec4c6
WOOBEEN <hwb0218@gmail.com>
2021-08-17 13:36:11 +0900

    style : theme color 추가
    Co-authored-by: imhjlov <imhjlov@users.noreply.github.com>

--- src/Styles/Theme.js
+++ src/Styles/Theme.js
@@ -1,3 +1,11 @@
 export const Theme = {
-  color: {},
+  color: {
+    darkmint: "#22A6B3",
+    lightmint: "#7ED6DF",
+    gray: "#C4C4C4",
+    darkgray: "#757575",
+    red: "#eb4d4b",
+    fontBlack: "#333333",
+    fontWhite: "#FFFFFF",
+  },
 };
